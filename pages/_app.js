f66940281318fc0e1af9b709857d3e76d9497e12
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-05 05:14:37 -0300

    Update GG Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,20 +5,21 @@ import GlobalStyle from '../src/theme/GlobalStyle';
 function MyApp({ Component, pageProps }){
     return (
         <>
-            {/* <!-- Google tag (gtag.js) --> */}
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-TLCDVMVGRC"></script>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-TLCDVMVGRC');
-                    `
-                }}  
-            />
             <Head>
+                {/* <!-- Google tag (gtag.js) --> */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-TLCDVMVGRC"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-TLCDVMVGRC');
+                        `
+                    }}  
+                />
+                
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
